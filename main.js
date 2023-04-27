6eb0b5c87b6c5f430fedda15fa4c29130d3d2d96
MAR0oo <87545100+MAR0oo@users.noreply.github.com>
2023-04-27 15:36:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function copyHTML(button) {
         @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
         body { font-family: 'Roboto', Arial, sans-serif; font-size: 16px; margin: 0; background-color: #f2f2f2; }
         table { border-collapse: collapse; }
-        .container { max-width: 600px; margin: 20px auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); }
+        .container { max-width: 800px; margin: 20px auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); }
         .header { background-color: #4caf50; padding: 20px; text-align: center; border-top-left-radius: 10px; border-top-right-radius: 10px; }
         .header h1 { color: #ffffff; font-weight: 700; margin: 0; }
         .content { padding: 20px; }
