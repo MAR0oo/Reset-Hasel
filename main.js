197be0a86637be4cd62670da75dd6218bf8ba2cd
MAR0oo <87545100+MAR0oo@users.noreply.github.com>
2023-04-27 15:15:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -26,8 +26,9 @@ function generatePassword() {
         }
         password += characters.charAt(Math.floor(Math.random() * characters.length));
 
-        passwords += `<div class="pass">${password}<button onclick="copyPassword(this)">Kopiuj</button></div>`;
+        passwords += `<div class="pass">${password}<button onclick="copyPassword(this)">Kopiuj</button>   <button id="sebamamalego" onclick="copyHTML(this)">Kopiuj HTML</button></div>`;    
     }
+
     document.getElementById("password").innerHTML = "" + passwords;
 
 }
@@ -35,6 +36,62 @@ function generatePassword() {
 function copyPassword(button) {
     const password = button.parentNode.firstChild.textContent;
     navigator.clipboard.writeText(password).then(() => {
+        button.classList.add("copied");
+    });
+}
+function copyHTML(button) {
+    const password = button.parentNode.firstChild.textContent;
+    const htmlTemplate = `
+    <!DOCTYPE html>
+    <html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Zmiana hasła</title>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+        body { font-family: 'Roboto', Arial, sans-serif; font-size: 16px; margin: 0; background-color: #f2f2f2; }
+        table { border-collapse: collapse; }
+        .container { max-width: 600px; margin: 20px auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); }
+        .header { background-color: #4caf50; padding: 20px; text-align: center; border-top-left-radius: 10px; border-top-right-radius: 10px; }
+        .header h1 { color: #ffffff; font-weight: 700; margin: 0; }
+        .content { padding: 20px; }
+        .footer { background-color: #f2f2f2; padding: 20px; text-align: center; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; }
+        .important { font-weight: 700; }
+        </style>
+    </head>
+    <body>
+        <table width="100%" cellpadding="0" cellspacing="0" role="presentation">
+            <tr>
+                <td align="center">
+                    <table class="container" cellpadding="0" cellspacing="0" role="presentation" style="width: 600px;">
+                        <tr>
+                            <td class="header" align="center">
+                                <h1>Zmiana hasła</h1>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="content">
+                                <p>Witaj,</p>
+                                <p>Twoje hasło zostało pomyślnie zmienione. Poniżej znajduje się Twoje nowe hasło:</p>
+                                <p class="important">${password}</p>
+                                <p>Prosimy o zalogowanie się na swoje konto i natychmiastowe zmienienie hasła na własne, unikalne hasło.</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="footer">
+                                <p>Prosimy nie odpowiadać na ten email. To jest automatycznie wygenerowana wiadomość.</p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </body>
+    </html>
+    `;
+
+    navigator.clipboard.writeText(htmlTemplate).then(() => {
         button.classList.add("copied");
     });
 }
